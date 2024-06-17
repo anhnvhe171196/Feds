98b4bc0c51838ccd4d3b9585d72bdc8e5af1dd3b
anhnvhe171196 <169164879+anhnvhe171196@users.noreply.github.com>
2024-06-17 20:21:26 +0700

    sửa phần chanProfile

--- src/java/vn/fpt/edu/models/Contact.java
+++ src/java/vn/fpt/edu/models/Contact.java
@@ -25,5 +25,45 @@ public class Contact {
         this.phone = phone;
         this.message = message;
     }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSuject() {
+        return suject;
+    }
+
+    public void setSuject(String suject) {
+        this.suject = suject;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
     
 }
