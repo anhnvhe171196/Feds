633a4a2b061b90fc2a2c2656b5d37a8ed276a007
anhnvhe171196 <169164879+anhnvhe171196@users.noreply.github.com>
2024-06-17 20:24:02 +0700

    Contact_dao

--- src/java/vn/fpt/edu/dals/Contact_DAO.java
+++ src/java/vn/fpt/edu/dals/Contact_DAO.java
@@ -4,10 +4,37 @@
  */
 package vn.fpt.edu.dals;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  *
  * @author admin
  */
-public class Contact_DAO {
+public class Contact_DAO extends DBContext {
+
+    public void insertFeedBack(int productId, String comment, int Rating, int billId, String img) {
+        String sql = "INSERT INTO [dbo].[FeedBack]\n"
+                + "           ([Product_id]\n"
+                + "           ,[Comment]\n"
+                + "           ,[Date]\n"
+                + "           ,[Rating]\n"
+                + "           ,[Bill_Id]\n"
+                + "           ,[Img])\n"
+                + "     VALUES\n"
+                + "           (?, ?, GETDATE(), ?, ?, ?)";
+        try {
+            PreparedStatement st = connection.prepareStatement(sql);
+            st.setInt(1, productId);
+            st.setString(2, comment);
+            st.setInt(3, Rating);
+            st.setInt(4, billId);
+            st.setString(5, img);
+            st.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
     
 }
